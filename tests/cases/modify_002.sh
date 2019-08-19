d33a5c87a28292c5212efd4f9161235dc7436cac
Andrius Merkys <andrius.merkys@gmail.com>
2019-08-19 16:38:38 +0300

    Adding one more test case.

---
+++ tests/cases/modify_002.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Data::Compare;
+use Data::Dumper;
+use OPTiMaDe::FilterParser;
+use Scalar::Util qw(blessed);
+
+$Data::Dumper::Sortkeys = 1;
+
+my $parser = new OPTiMaDe::FilterParser;
+my $tree = $parser->parse_string( 'value.list HAS ALL "a", "b", "c"' );
+
+my $tree_now = OPTiMaDe::FilterParser::modify( $tree,
+    sub {
+        my( $node ) = @_;
+        if( blessed $node && $node->isa( OPTiMaDe::FilterParser::ListComparison:: ) ) {
+            my @values = @{$node->{values}};
+            my $node_now;
+            while( @values ) {
+                my( undef, $value ) = @{shift @values};
+                my $comparison = OPTiMaDe::FilterParser::Comparison->new( 'CONTAINS' );
+                $comparison->push_operand( $node->{property} );
+                $comparison->push_operand( $value );
+                if( $node_now ) {
+                    $node_now = [ $node_now, 'AND', $comparison ];
+                } else {
+                    $node_now = $comparison;
+                }
+            }
+            $node = $node_now;
+        }
+        return $node;
+    } );
+
+print Dumper $tree_now;
